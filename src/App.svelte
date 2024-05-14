5e1ad92b4a5b0cfa5e7c7d3697f1a165a667cdea
Thuan Nguyen <huuthuan.nguyen@hotmail.com>
2024-05-14 21:42:58 +0700

    init project

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Product from "./Product.svelte";
+  const products = [
+    {
+      id: "p1",
+      title: "A book",
+      price: 9.99,
+    },
+  ];
+</script>
+
+{#each products as product}
+  <Product
+    {...product}
+    on:add-to-cart={() => alert("Add to cart!")}
+    on:delete={() => alert("Delete!")}
+  />
+{/each}
---
+++ src/Product.svelte
@@ -0,0 +1,28 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+    export let title;
+    export let price;
+    export let bestseller = false;
+    export let id = "";
+    const dispatch = createEventDispatcher();
+
+    function addToCart(event) {
+        console.log(event);
+        dispatch("add-to-cart");
+    }
+
+    function deleteProduct(event) {
+        console.log(event);
+        dispatch("delete");
+    }
+</script>
+
+<article>
+    <h1>{title}</h1>
+    <h2>${price}</h2>
+    {#if bestseller}
+        <h3>{bestseller}</h3>
+    {/if}
+    <button on:click={addToCart}>Add to cart</button>
+    <button on:click={deleteProduct}>Delete</button>
+</article>
